b3dc5279598a894fc3af1688cf5ac9a691556000
noaalfia <noaalfia2010@gmail.com>
2026-01-09 15:48:03 +0200

    base of run1

---
+++ run1.py
@@ -0,0 +1,19 @@
+from moving_profile import moving_profile
+from turn import turn
+from new import * 
+
+#turn(90, False, kp=13.6, kd=0.6, ki = 0.1)
+#print(hub.imu.heading())
+
+
+#turn(180, False, kp=10, kd=0.35, ki=0)
+#print(hub.imu.heading())
+
+#turn(45, False, kp=12)
+#print(hub.imu.heading())
+
+#turn(30, False)
+#print(hub.imu.heading())
+
+turn(15, False)
+print(hub.imu.heading())
