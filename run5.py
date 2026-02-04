cf644012a8ffd8ab367abba75c55758a35f996d4
Daniellev8812 <danielle.vulkan1@gmail.com>
2026-02-04 23:41:24 +0200

    Update run5.py

--- run5.py
+++ run5.py
@@ -1,18 +1,19 @@
-from moving_profile import moving_profile
+from new import *
 from turn import turn
-from new import * 
+from moving_profile import moving_profile
 
-moving_profile(230,1000,950,0)
+moving_profile(260,1000,950,0)
 turn(-35)
 right_arm.run_angle(1000, -700, Stop.BRAKE, False) 
-moving_profile(640, 700, 650, -40)
-right_arm.run_angle(1000, 700) 
+moving_profile(515, 500, 450, -35)
 
 turn(-82)
-moving_profile(725, 1000, 950, -90, kp=7.5)
-turn(-160)
-moving_profile(70,1000,950,-160)
-left_arm.run_angle(1000, 700)
-moving_profile(60, 1000, 950, -160)
-moving_profile(60, -1000, 950, -160)
- 
+moving_profile(680, 1000, 950, -90, kp=7.5) 
+turn(-155)
+left_arm.run_angle(1000, 700, Stop.BRAKE, False)
+moving_profile(40, 700, 650, -155)
+moving_profile(100, 900, 850, -170)
+
+moving_profile(60, -500, 450, -160)
+turn(-92)
+moving_profile(224, 900, 850, -88)
